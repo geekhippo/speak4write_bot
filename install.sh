9dc0798db64cbd607fbb22b39aad134debaa132c
root <root@hungry-bell.1cent.network>
2026-04-24 06:32:40 +0500

    Use docker-compose instead of docker compose

--- install.sh
+++ install.sh
@@ -30,10 +30,11 @@ curl -sSL https://raw.githubusercontent.com/geekhippo/speak4write_bot/master/Doc
 curl -sSL https://raw.githubusercontent.com/geekhippo/speak4write_bot/master/requirements.txt -o requirements.txt
 curl -sSL https://raw.githubusercontent.com/geekhippo/speak4write_bot/master/docker-compose.yml -o docker-compose.yml
 
-# 5. Сборка и запуск через docker compose
-echo "🏗️ Собираю и запускаю бота через docker compose..."
-docker compose pull
-docker compose up --detach --build
+# 5. Запуск через docker-compose
+echo "🏗️ Собираю и запускаю бота..."
+# Используем docker-compose (через дефис), так как это стандарт для старых версий
+docker-compose build
+docker-compose up -d
 
 echo "🎉 Готово! Бот @speak4write_bot запущен."
-echo "Логи: docker compose logs -f"
+echo "Логи: docker-compose logs -f"
